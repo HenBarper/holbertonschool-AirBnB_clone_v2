c92258baf352ec6c88d95e3e847e17bc1a7a49ad
gumquat <gumquat@gmail.com>
2023-07-03 14:39:58 -0500

    hey you cant stop this hahaha

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+import os
 
+"""conditional that checks the value of the environment table"""
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 
-storage = FileStorage()
 storage.reload()
