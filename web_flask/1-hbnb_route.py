9a82705610f7ec5e292f93ad95c3f4571cde7a88
gumquat <gumquat@gmail.com>
2023-07-13 14:03:46 -0500

    indent fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,6 +4,7 @@ script that starts a Flask web application:
 Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
+/hbnb: display "HBNB"
 You must use the option strict_slashes=False in your route definition
 """
 from flask import Flask
@@ -15,9 +16,11 @@ app = Flask(__name__)
 def hello_hbnb():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
